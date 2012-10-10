d0d587e8a633bbbdda4ef90ec3c5365cf906dc67
Oliver Yang <oyang@sugarcrm.com>
2012-10-10 12:41:30 -0700

    Update the users list

--- appconfig.py
+++ appconfig.py
@@ -1,2 +1,22 @@
 site_url='http://honey-g/BranchBuilder'
 jenkins_url='http://honey-g:8080'
+od_users = {    "betauser1": "Beta User1", \
+		"betauser2": "Beta User2", \
+		"cfox": "Chinghua Fox", \
+		"dsafar": "David Safar", \
+		"dclunie": "David Clunie", \
+		"dganti": "Deepthi Ganti", \
+		"egan": "Emily Gan", \
+		"eyang": "Eric Yang", \
+		"franklin": "Fanklin Liu", \
+		"gpatterson": "Geno Patterson", \
+		"grelampagos": "Gino Relampagos", \
+		"jchen": "Jackie Chen", \
+		"jcho": "Jessica Cho", \
+		"jxia": "Jennifer Xia", \
+		"mlouis": "Mazen Louis", \
+		"oyang": "Oliver Yang", \
+		"rlee": "Randy Lee", \
+		"rzhou": "Ran Zhou", \
+		"phuang": "Paul Huang", \
+	}
