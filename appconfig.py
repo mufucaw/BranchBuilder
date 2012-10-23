baee992b2e8c63b91bb4d03def8ecadd90e5779a
Oliver Yang <oyang@sugarcrm.com>
2012-10-22 19:39:35 -0700

    Add edit function for deployment

--- appconfig.py
+++ appconfig.py
@@ -1,23 +1,23 @@
 site_url='http://honey-g/BranchBuilder'
 jenkins_url='http://honey-g:8080'
-od_users = {    "betauser1": "Beta User1", \
-		"betauser2": "Beta User2", \
-		"cfox": "Chinghua Fox", \
-		"dsafar": "David Safar", \
-		"dclunie": "David Clunie", \
-		"dganti": "Deepthi Ganti", \
-		"egan": "Emily Gan", \
-		"eyang": "Eric Yang", \
-		"franklin": "Fanklin Liu", \
-		"gpatterson": "Geno Patterson", \
-		"grelampagos": "Gino Relampagos", \
-		"jchen": "Jackie Chen", \
-		"jcho": "Jessica Cho", \
-		"jxia": "Jennifer Xia", \
-		"mlouis": "Mazen Louis", \
-		"oyang": "Oliver Yang", \
-		"rlee": "Randy Lee", \
-		"rzhou": "Ran Zhou", \
-		"phuang": "Paul Huang", \
+od_users = {    "betauser1": {"full_name": "Beta User1", "email": "quality-assurance@sugarcrm.com"} ,\
+		"betauser2": {"full_name": "Beta User2", "email": "quality-assurance@sugarcrm.com"} ,\
+		"cfox": {"full_name": "Chinghua Fox", "email": "cfox@sugarcrm.com"} ,\
+		"dsafar": {"full_name": "David Safar", "email": "dsafar@sugarcrm.com"} ,\
+		"dclunie": {"full_name": "David Clunie", "email": "dclunie@sugarcrm.com"} ,\
+		"dganti": {"full_name": "Deepthi Ganti", "email": "dganti@sugarcrm.com"} ,\
+		"egan": {"full_name": "Emily Gan", "email": "egan@sugarcrm.com"} ,\
+		"eyang": {"full_name": "Eric Yang", "email": "eyang@sugarcrm.com"} ,\
+		"franklin": {"full_name": "Fanklin Liu", "email": "franklin@sugarcrm.com"} ,\
+		"gpatterson": {"full_name": "Geno Patterson", "email": "gpatterson@sugarcrm.com"} ,\
+		"grelampagos": {"full_name": "Gino Relampagos", "email": "gpatterson@sugarcrm.com"} ,\
+		"jchen": {"full_name": "Jackie Chen", "email": "jchen@sugarcrm.com"} ,\
+		"jcho": {"full_name": "Jessica Cho", "email": "jcho@sugarcrm.com"} ,\
+		"jxia": {"full_name": "Jennifer Xia", "email": "jxia@sugarcrm.com"} ,\
+		"mlouis": {"full_name": "Mazen Louis", "email": "mlouis@sugarcrm.com"} ,\
+		"oyang": {"full_name": "Oliver Yang", "email": "oyang@sugarcrm.com"} ,\
+		"rlee": {"full_name": "Randy Lee", "email": "rlee@sugarcrm.com"} ,\
+		"rzhou": {"full_name": "Ran Zhou", "email": "rzhou@sugarcrm.com"} ,\
+		"phuang": {"full_name": "Paul Huang", "email": "phuang@sugarcrm.com"} ,\
 	}
 od_version = ['6.6.0']
