37f5b2807faa1b6644ca79152f0acadb594d4c08
Oliver Yang <oyang@sugarcrm.com>
2013-10-03 10:15:06 -0700

    Fix the branch value bug

--- versionconfig.py
+++ versionconfig.py
@@ -1,5 +1,5 @@
 branchbuilder= {
-	'7.0.0': [
+	'7.1.0': [
 		{
 			'repo_name': 'Mango',
 			'repo_url': 'git@github.com:sugarcrm/Mango.git',
@@ -8,7 +8,7 @@ branchbuilder= {
 		{
 			'repo_name': 'Sidecar',
 			'repo_url': 'git@github.com:sugarcrm/sidecar.git',
-			'branch': ''
+			'branch': 'master'
 		},
 		{
 			'repo_name': 'Styleguide',
@@ -16,55 +16,55 @@ branchbuilder= {
 			'branch': 'master'
 		}
 	],
-	'7.1.0': [
-		{
-			'repo_name': 'Mango',
-			'repo_url': '',
-			'branch': ''
-		},
-		{
-			'repo_name': 'Sidecar',
-			'repo_url': '',
-			'branch': ''
-		},
-		{
-			'repo_name': 'Styleguide',
-			'repo_url': '',
-			'branch': ''
-		}
-	],
 	'7.0.1': [
 		{
 			'repo_name': 'Mango',
-			'repo_url': '',
-			'branch': ''
+			'repo_url': 'git@github.com:sugarcrm/Mango.git',
+			'branch': '7_0_x'
 		},
 		{
 			'repo_name': 'Sidecar',
-			'repo_url': '',
-			'branch': ''
+			'repo_url': 'git@github.com:sugarcrm/sidecar.git',
+			'branch': '7_0_x'
 		},
 		{
 			'repo_name': 'Styleguide',
-			'repo_url': '',
-			'branch': ''
+			'repo_url': 'git@github.com:sugarcrm/styleguide.git',
+			'branch': '7_0_x'
 		}
 	],
-	'6.7.4':[
+	'6.7.4': [
 		{
 			'repo_name': 'Mango',
-			'repo_url': '',
-			'branch': ''
+			'repo_url': 'git@github.com:sugarcrm/Mango.git',
+			'branch': '6_7_4'
 		},
 		{
 			'repo_name': 'Sidecar',
-			'repo_url': '',
-			'branch': ''
+			'repo_url': 'git@github.com:sugarcrm/sidecar.git',
+			'branch': '6_7_4'
 		},
 		{
 			'repo_name': 'Styleguide',
-			'repo_url': '',
-			'branch': ''
+			'repo_url': 'git@github.com:sugarcrm/styleguide.git',
+			'branch': '6_7_4'
+		}
+	],
+	'6.5.16': [
+		{
+			'repo_name': 'Mango',
+			'repo_url': 'git@github.com:sugarcrm/Mango.git',
+			'branch': '6_5_16'
+		},
+		{
+			'repo_name': 'Sidecar',
+			'repo_url': 'git@github.com:sugarcrm/sidecar.git',
+			'branch': '6_5_16'
+		},
+		{
+			'repo_name': 'Styleguide',
+			'repo_url': 'git@github.com:sugarcrm/styleguide.git',
+			'branch': '6_5_16'
 		}
 	]
 }
